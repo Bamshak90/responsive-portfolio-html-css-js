6f06e66040a368be5b12f5d50f7e3e0c5f518842
Bamshak90 <lilmarkhat@gmail.com>
2023-11-23 08:27:31 +0000

    Added the hamburger menu and also started making the page responsive. Added javascript to make the hamburger menu open and close on toggle

---
+++ script.js
@@ -0,0 +1,6 @@
+function togglemenu() {
+    const menu = document.querySelector(".menu-links");
+    const icon = document.querySelector(".hamburger-icons");
+    menu.classList.toggle("open");
+    icon.classList.toggle("open");
+}
